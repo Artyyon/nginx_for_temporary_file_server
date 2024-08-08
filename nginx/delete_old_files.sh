257ab911edb3d1d7f5d7abedccc6c8cb7a38ff5b
Arthur Paes Leme Stiegler <arthurp.l.stiegler@gmail.com>
2024-08-08 08:50:54 -0300

    Auto delete rule bug fix.

--- nginx/delete_old_files.sh
+++ nginx/delete_old_files.sh
@@ -3,5 +3,11 @@
 # Diretório onde os arquivos são armazenados
 UPLOAD_DIR="/usr/share/nginx/html/files"
 
+# Log de execução
+echo "$(date) - Executando script de exclusão" >> /var/log/nginx/delete_files.log
+
+# Verificar o diretório e os arquivos
+ls -l "$UPLOAD_DIR" >> /var/log/nginx/delete_files.log
+
 # Encontrar e excluir arquivos com mais de 10 minutos
-find "$UPLOAD_DIR" -type f -mmin +10 -exec rm -f {} \;
+find "$UPLOAD_DIR" -type f -mmin +10 -exec rm -f {} \; 2>> /var/log/nginx/delete_files.log
